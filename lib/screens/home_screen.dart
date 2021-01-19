fff6bd7b7d63861c48967138f7e119038a0513af
Neil Ye <neil.ye@cloud-interactive.com>
2021-01-19 15:00:53 +0800

    6. Recent Orders

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_food_delivery_ui/widgets/recent_orders.dart';
 
 import '../data/data.dart';
 
@@ -62,7 +63,8 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
               ),
             ),
-          )
+          ),
+          RecentOrders(),
         ],
       ),
     );
